652f61a31e6874409c8e5bc262ae5f0be843355b
hikiman <skyainq@naver.com>
2024-02-16 15:01:41 +0900

    React 중급 문법 활용

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import data from './data.js';
 import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
 import Detail from './routes/Detail.js';
+import axios from 'axios';
 
 function App() {
 
@@ -38,14 +39,25 @@ function App() {
             </div>
             <Button variant="secondary" size="sm" onClick={() => {
               let copy = [...members];
-              copy.sort(function(a, b){
-                if(a.title < b.title) return -1; 
-                if(a.title > b.title) return 1; 
-                if(a.title === b.title) return 0; 
+              copy.sort(function (a, b) {
+                if (a.title < b.title) return -1;
+                if (a.title > b.title) return 1;
+                if (a.title === b.title) return 0;
               })
               setMembers(copy);
             }}>
-              Small button
+              정렬하기
+            </Button>
+            <Button variant='primary' size='sm' onClick={() => {
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+                .then((res) => {
+                  let copy = [...members, ...res.data]
+                  // let copy = [...members];
+                  // let copyPlus = copy.concat(res.data);
+                  setMembers(copy);
+                })
+            }}>
+              더보기
             </Button>
           </>
         } />
@@ -56,9 +68,12 @@ function App() {
 }
 
 function Characters(props) {
+
+  let navigate = useNavigate();
+
   return (
     <div class="col-md-4">
-      <img src={process.env.PUBLIC_URL + `/myImg/prod${props.member['id'] + 1}.png`} width='100%' alt={props.member.title} />
+      <img src={process.env.PUBLIC_URL + `/myImg/prod${props.member['id']}.png`} width='100%' alt={props.member.title} onClick={() => { navigate(`/detail/${props.member["id"]}`) }} />
       <h4>{props.member["title"]}</h4>
       <p>{props.member["content"]}</p>
     </div>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,20 +1,39 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useAsyncError, useParams } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 function Detail(props) {
 
+    let [tab, setTab] = useState(0);
     let { id } = useParams();
-    
-    if (id > props.members.length){
+    if (id > props.members.length || isNaN(id)) {
         id = 0;
     }
-
     let member = props.members.find(member => member['id'] == id);
+    let [fade, setFade] = useState('')
+
+    useEffect(() => {
+        let timer = setTimeout(() =>{
+            setFade('end')
+        }, 100);
+        return() => {
+            clearTimeout(timer);
+            setFade('');
+        };
+    }, [member]); 
+
+// Q. setTimeout 왜 씁니까
+// 리액트 18버전 이상부터는 automatic batch 라는 기능이 생겼습니다.
+// state 변경함수들이 연달아서 여러개 처리되어야한다면 state 변경함수를 다 처리하고 마지막에 한 번만 재렌더링됩니다. 
+// 그래서 'end' 로 변경하는거랑 ' ' 이걸로 변경하는거랑 약간 시간차를 뒀습니다.
+// 찾아보면 setTimeout 말고 flushSync() 이런거 써도 될 것 같기도 합니다. automatic batching을 막아줍니다.
+
 
     return (
-        <div className="container">
+        <div className={`container start ${fade}`}>
             <div className="row">
                 <div className="col-md-6">
-                    <img src={process.env.PUBLIC_URL + `/myImg/prod${member['id'] + 1}.png`} width='100%' alt={member.title} />
+                    <img src={process.env.PUBLIC_URL + `/myImg/prod${member['id']}.png`} width='100%' alt={member.title} />
                 </div>
                 <div className="col-md-6">
                     <h4 className="pt-5">{member.title}</h4>
@@ -23,7 +42,41 @@ function Detail(props) {
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link eventKey="link0" onClick={() => {
+                        setTab(0);
+                    }}>상세정보</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link1" onClick={() => {
+                        setTab(1);
+                    }}>리뷰</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link2" onClick={() => {
+                        setTab(2);
+                    }}>반품/교환정보</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <TabContent tab={tab} />
         </div>
+    )
+}
+
+function TabContent({ tab }) {
+
+    // if (tab == 0){
+    //     return <div>상세정보</div>
+    // }
+    // if (tab == 1){
+    //     return <div>리뷰</div>
+    // }
+    // if (tab == 2){
+    //     return <div>반품/교환정보</div>
+    // }
+    return (
+        [<div>상세정보</div>, <div>리뷰</div>, <div>반품/교환정보</div>][tab]
     )
 }
 
